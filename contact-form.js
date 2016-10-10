31883d8f0cb88594e9515cea342d2b24f0915b64
alexrewrew <alexrewrew@live.com>
2016-10-10 12:01:54 +0300

    changes

--- contact-form.js
+++ contact-form.js
@@ -5,22 +5,26 @@ $(document).ready(function () {
         if ($("#name").val() == "") {
             send = false;
             $("#name").addClass("errorForm");
+            $('.send_error').show('fast');
         }
         if ($("#email").val() == "") {
             send = false;
             $("#email").addClass("errorForm");
+            $('.send_error').show('fast');
         }
         else {
             var r = /^\w+@\w+\.\w{2,4}$/i;
             if (!r.test($("#email").val())) {
                 send = false;
                 $("#email").addClass("errorForm");
+                $('.send_error').show('fast');
             }
 
         }
         if ($("#message").val() == "") {
             send = false;
             $("#message").addClass("errorForm");
+            $('.send_error').show('fast');
         }
         
         if (send) {
@@ -34,7 +38,9 @@ $(document).ready(function () {
                 data: {name: name, email: email, message: message},
                 success: function (data) {
                     if (data == "") {
-                        window.location.href = "index.html";//edit start page of site
+                        /*window.location.href = "index.html";//edit start page of site*/
+                        $('.send_error').hide('fast');
+                        $('.send_success').show('fast');
                     }
                 }
             });
