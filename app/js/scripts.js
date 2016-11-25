b396febcff402e7a7f58fca47375e0a77765cdad
alexrewrew <alexrewrew@MacBook-Pro-Oleksandr.local>
2016-11-26 01:38:28 +0200

    26go

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,25 +1,34 @@
-$(document).ready(function(){
+$(document).ready(function () {
     //menu
-    $('#nav-icon').click(function(){
+    $('.menu').click(function () {
         $('#nav-icon').toggleClass('open');
         $('.main_menu').toggleClass('opened');
+    });
+
+
+    $('.menu_list li a').click(function () {
+        $('#nav-icon').removeClass('open');
+        $('.main_menu').removeClass('opened');
+
     });
 
     $(window).on('load resize', function () {
         $(window).scroll(function () {
             if ($(this).scrollTop() > 500) {
                 $("#nav-icon").addClass('nav-down');
-                /*$("#up").css('display','block');*/
             } else {
                 $("#nav-icon").removeClass('nav-down');
-               /* $("#up").css('display','none');*/
             }
-            /*if ($(this).scrollTop() > 650) {
-                $("#burger2").attr("src", "assets/img/icons/Burger_mob2.svg");
-            } else {
-                $("#burger2").attr("src", "assets/img/icons/Burger_mob1.svg");
-            }*/
         });
+    });
+
+    $(document).mouseup(function (e) {
+        var div = $(".opened");
+        if (!div.is(e.target) &&
+            div.has(e.target).length === 0) {
+            $('#nav-icon').removeClass('open');
+            $('.main_menu').removeClass('opened');
+        }
     });
 
     //slide anchors
@@ -30,5 +39,34 @@ $(document).ready(function(){
         $('body,html').animate({
             scrollTop: top
         }, 1500);
+    });
+    // parallax
+    if ($(window).width() > '1200') {
+
+        $.fn.moveIt = function () {
+            var $window = $(window);
+            var instances = [];
+            $(this).each(function () {
+                instances.push(new moveItItem($(this)));
+            });
+            window.onscroll = function () {
+                var scrollTop = $window.scrollTop();
+                instances.forEach(function (inst) {
+                    inst.update(scrollTop);
+                });
+            }
+        }
+        var moveItItem = function (el) {
+            this.el = $(el);
+            this.speed = parseInt(this.el.attr('data-scroll-speed'));
+        };
+        moveItItem.prototype.update = function (scrollTop) {
+            var pos = scrollTop / this.speed;
+            this.el.css('transform', 'translateY(' + -pos + 'px)');
+        };
+    }
+    ;
+    $(function () {
+        $('[data-scroll-speed]').moveIt();
     });
 });
